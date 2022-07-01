5106518be5a1a42454508ec02155a5b1cd7d493b
Oli <oliverqiu@outlook.com>
2022-07-01 18:19:47 +1000

    feat: enable fp16

--- parlai/tasks/redial/train_transformer_rec.py
+++ parlai/tasks/redial/train_transformer_rec.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
         task='redial',
         model='transformer_rec/generator',
         model_file='saved/transformer_rec',
+        fp16=True,
         dict_tokenizer='nltk',
         dict_lower=True,
         batchsize=64,
